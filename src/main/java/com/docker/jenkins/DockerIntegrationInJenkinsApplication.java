3a76128cf2fc5735485ed9311581b8e71f81dc6b
Sreenivasa Reddy <msreenivasareddy2018@gmail.com>
2022-05-04 18:35:10 +0530

    Initial commit

---
+++ src/main/java/com/docker/jenkins/DockerIntegrationInJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.docker.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerIntegrationInJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerIntegrationInJenkinsApplication.class, args);
+	}
+
+}
